79ac1dd3b3c3e874f14988d4583bc029ab368423
John Kalinauskas <jkalinauskas@live.com>
2018-03-29 19:23:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,14 @@
 
+colors = ["Red", "Orange", "Yellow", "Green:", "Blue:", "Indigo, "Violet:"]
 
+
+
+puts "R #{colors[0]"
+puts "O #{colors[1]"
+puts "Y #{colors[2]"
+puts "G #{colors[3]"
+puts "B #{colors[4]"
+puts "I #{colors[5]"
+puts "V #{colors[6]"
 
 
