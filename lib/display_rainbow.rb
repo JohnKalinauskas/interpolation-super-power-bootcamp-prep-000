c93ab76320d7afc418466960731ee7744b79ec2d
John Kalinauskas <jkalinauskas@live.com>
2018-03-29 19:45:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
-
+letters = ["Red","Orange","Yellow", "Green", "Blue", "Indigo", "Violet"]
+puts "R: #{letters[0]}"
+puts "O: #{letters[1]}"
+puts "Y: #{letters[2]}"
+puts "G: #{letters[3]}"
+puts "B: #{letters[3]}"
+puts "I: #{letters[5]}"
+puts "V: #{letters[6]}"
